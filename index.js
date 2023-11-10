a14580fe401c1befa7ac95cdf885c8e787c6ab96
Rodika Aryku <119984393+roaryku@users.noreply.github.com>
2023-11-09 18:22:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// const image = document.querySelector('#image');
-// const button = document.querySelector('#submit');
-
-// button.addEventListener('click', () => {
-//     image.style.display = 'none'
-// })
-
-// const books = ["The women's heart", "Braking free", "Chasing the vines"];
-// const check = books.includes('Braking free') ? 'It is here' : 'It is not here'
-// console.log(check)
-
-// const sentence = 'I love Mike';
-// const word = sentence.startsWith('I');
-// const end = sentence.endsWith('Bob')
-// console.log(end)
-
 const inputField = document.querySelector('#search');
 const divs = document.querySelectorAll('.places');
 
