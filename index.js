70e3fded2ca0872990d6f368ebb5dd4c81a3b440
Rodika Aryku <119984393+roaryku@users.noreply.github.com>
2023-11-11 09:22:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ inputField.addEventListener('keyup', (e) => {
     const word =  e.target.value.toLowerCase();
      
      divs.forEach(item => {
-            item.querySelector('h3').textContent.toLowerCase().includes(word) ||
+            item.querySelector('h3').textContent.toLowerCase().includes(word) 
             item.querySelector('p').textContent.toLowerCase().includes(word) 
             ? (item.style.display = 'block') 
             : (item.style.display = 'none')
